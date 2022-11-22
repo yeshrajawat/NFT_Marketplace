59ce34453e2be73a9fd89192424025e5daa501dd
yeshrajawat <yeshrajawat@gmail.com>
2022-11-23 01:44:18 +0530

    Initial setup completed now working on navBar

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect, useContext } from 'react';
+
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+
+const Navbar = () => (
+  <div>Navbar</div>
+);
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../components';
+import '../styles/globals.css';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/6a68b88619.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
