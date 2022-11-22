d793ac3dad1552e5c3b6aefc5b4c280637b63d05
yeshrajawat <yeshrajawat@gmail.com>
2022-11-23 03:23:40 +0530

    Added Nav Bar

--- pages/create-nft.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CreateNFT = () => (
-  <div>create-nft</div>
-);
-
-export default CreateNFT;
--- pages/my-nft.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyNFT = () => (
-  <div>myNFT</div>
-);
-
-export default MyNFT;
